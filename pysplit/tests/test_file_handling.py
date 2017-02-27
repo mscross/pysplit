fb31731d984bea69553e1ea8ff746fddf30c8401
Joshua Warner <silvertrumpet999@gmail.com>
2017-02-27 01:59:45 -0600

    TST: Add test for bad files/nonexistant files.

---
+++ pysplit/tests/test_file_handling.py
@@ -0,0 +1,24 @@
+import numpy as np
+from numpy.testing import assert_raises
+from pysplit import make_trajectorygroup
+
+
+def test_bogus_signature():
+    signature = './nothing_to_see_here*'
+    assert_raises(LookupError, make_trajectorygroup, signature)
+
+
+def test_bogus_filenames():
+    filenames = ['./not_a_trajectory0',
+                 './not_a_trajectory1',
+                 './not_a_trajectory2']
+    assert_raises(IOError, make_trajectorygroup, filenames)
+
+
+def test_non_trajectory():
+    signature = './test*'
+    assert_raises(IOError, make_trajectorygroup, signature)
+
+
+if __name__ == '__main__':
+    np.testing.run_module_suite()
